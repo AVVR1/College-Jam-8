59cb3c2ee189f15d29c9ad04082d3484a6a3df8e
AVVR1 <nopolanopo@gmail.com>
2025-03-17 12:07:48 +0200

    StateManager ja post processing

--- College Jam/Assets/Scripts/StateManager.cs
+++ College Jam/Assets/Scripts/StateManager.cs
@@ -1,21 +1,26 @@
+using Cinemachine.PostFX;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class StateManager : MonoBehaviour
 {
 	public enum State { present, past }
 	public State currentState = State.present;
+	[SerializeField] PostProcessVolume postProcessVolume;
 
 	private void Update()
 	{
 		if (currentState == State.present)
 		{
-
+			// mustavalkoinen pois p‰‰lt‰
+			postProcessVolume.enabled = false;
 		}
 		else
 		{
-
+			//mustavalkoinen p‰‰lle
+			postProcessVolume.enabled = true;
 		}
 	}
 }
