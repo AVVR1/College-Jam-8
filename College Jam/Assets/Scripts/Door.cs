79ee3b34db5e600286da6570285b14f7ee5bcd90
AVVR1 <nopolanopo@gmail.com>
2025-03-18 13:59:55 +0200

    Avain, Lukko, Ovi
    FINALLY. avain lukko ovi systeemi jossa voi nostaa avaimen ja pudottaa sen, avaimella voi avata lukkoja jotka ovat samaa ID:tä joka avaa saman ID:n oven.

---
+++ College Jam/Assets/Scripts/Door.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+	public StateManager.State state;
+	public int ID;
+}
--- College Jam/Assets/Scripts/Player.cs
+++ College Jam/Assets/Scripts/Player.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 	float sprintMultiplier = 1;
 	public float moveSpeed;
-	Rigidbody rb;
+
 	[SerializeField] Camera cam;
+
+	Rigidbody rb;
+
+	public int selectedKeyID = -1;
+
+	StateManager statemanager;
 
 	private void Awake()
 	{
+		statemanager = FindAnyObjectByType<StateManager>();
 		rb = GetComponent<Rigidbody>();
 	}
 
@@ -22,6 +30,8 @@ public class Player : MonoBehaviour
     void Update()
 	{
 		Movement();
+		MouseInput();
+		DropInput();
 	}
 
 	private void Movement()
@@ -49,5 +59,129 @@ public class Player : MonoBehaviour
 			moveDirRight.y = 0f;
 			rb.velocity = (moveDirRight.normalized * horizontal + moveDirForward.normalized * vertical) * moveSpeed * sprintMultiplier + Vector3.up * rb.velocity.y;
 		}
+	}
+
+	public Collider Raycast()
+	{
+		RaycastHit hit;
+		Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 3);
+		if (hit.collider != null)
+		{
+			return hit.collider;
+		}
+		return null;
+	}
+
+	private void PickupKey(Collider rayCollider)
+	{
+		selectedKeyID = rayCollider.GetComponent<Key>().ID;
+		rayCollider.transform.parent = gameObject.transform;
+		List<GameObject> list = GetStateObjectsList(rayCollider.GetComponent<Key>().state);
+		list.Remove(rayCollider.gameObject);
+		rayCollider.GetComponent<Rigidbody>().isKinematic = true;
+		rayCollider.transform.localPosition = new Vector3(0, 0.3f, 1);
+	}
+
+	private void DropKey()
+	{
+		GameObject parent = null;
+		Transform child = transform.GetChild(0);
+
+		parent = GetStateObjectsParent(statemanager.currentState);
+		child.parent = parent.transform;
+		GetStateObjectsList(statemanager.currentState).Add(child.gameObject);
+		child.GetComponent<Rigidbody>().isKinematic = false;
+		selectedKeyID = -1;
+	}
+
+	private void Interact(Collider rayCollider)
+	{
+		if (selectedKeyID == rayCollider.GetComponent<Lock>().ID)
+		{
+			print($"Open door {rayCollider.GetComponent<Lock>().ID}");
+
+			//find door with correct ID
+			GameObject[] objects = GameObject.FindGameObjectsWithTag("Door");
+			foreach (GameObject obj in objects)
+			{
+				Door door = obj.GetComponent<Door>();
+				if (door != null && door.ID == rayCollider.GetComponent<Lock>().ID)
+				{
+					//correct door found
+					GameObject target = obj;
+					//destroy door
+					Destroy(target);
+					//remove door
+					List<GameObject> list = GetStateObjectsList(door.state);
+					list.Remove(target);
+					//destroy key
+					Destroy(transform.GetChild(0).gameObject);
+					
+					break;
+				}
+			}
+		}
+	}
+
+	private void MouseInput()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Collider rayCollider = Raycast();
+			if (rayCollider != null)
+			{
+				if (rayCollider.CompareTag("Key"))
+				{
+					if (selectedKeyID == -1)
+					{
+						PickupKey(rayCollider);
+					}
+					else
+					{
+						DropKey();
+						PickupKey(rayCollider);
+					}
+				}
+				else if (rayCollider.CompareTag("Lock"))
+				{
+					Interact(rayCollider);
+				}
+			}
+		}
+	}
+
+	private void DropInput()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			if (transform.GetChild(0) != null)
+			{
+				DropKey();
+			}
+		}
+	}
+
+	private GameObject GetStateObjectsParent(StateManager.State state)
+	{
+		switch (state)
+		{
+			case StateManager.State.present:
+			return statemanager.presentObjectsParent;
+			case StateManager.State.past:
+			return statemanager.pastObjectsParent;
+		}
+		return null;
+	}
+
+	private List<GameObject> GetStateObjectsList(StateManager.State state)
+	{
+		switch (state)
+		{
+			case StateManager.State.present:
+			return statemanager.presentObjects;
+			case StateManager.State.past:
+			return statemanager.pastObjects;
+		}
+		return null;
 	}
 }
