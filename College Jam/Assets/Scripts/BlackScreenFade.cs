6b1fdd9644a1b4859c106a4c9a693b063b22bcad
AVVR1 <nopolanopo@gmail.com>
2025-03-20 10:25:15 +0200

    ScreenFade

--- College Jam/Assets/Scripts/BlackScreenFade.cs
+++ College Jam/Assets/Scripts/BlackScreenFade.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlackScreenFade : MonoBehaviour
 {
+	[SerializeField] GameObject BlackPanel;
 	private void OnEnable()
 	{
 		PressurePlate.onStateSwitch += FadeScreen;
@@ -17,11 +19,21 @@ public class BlackScreenFade : MonoBehaviour
 	private void FadeScreen()
 	{
 		StartCoroutine(FadePanel());
-		
 	}
 
 	private IEnumerator FadePanel()
 	{
+		BlackPanel.SetActive(true);
+		float alpha = 1.0f;
+		Image blackPanelImage = BlackPanel.GetComponent<Image>();
+		blackPanelImage.color = new Color(0, 0, 0, 1);
+		while (blackPanelImage.color.a > 0)
+		{
+			alpha -= Time.deltaTime / 2;
+			blackPanelImage.color = new Color(0, 0, 0, alpha);
+			yield return null;
+		}
+		BlackPanel.SetActive(false);
 		yield return null;
 	}
 }
